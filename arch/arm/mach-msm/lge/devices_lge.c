5b89e4022fa892cda221493b222d038853d7af08
Vojtech Bocek <vbocek@gmail.com>
2013-12-06 22:56:29 +0100

    Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to hammerhead, it is based of my mako/grouper port, which is
      based of Asus TF201 patch ported by Jens Andersen <jens.andersen@gmail.com>
    
      Hammerhead is the first device I've ported this patch to which uses
      device tree binary instead of atags. It is also rather picky - its
      bootloader erases first 128MB of RAM on reboot, so following hacks had
      to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      Same as my mako/flo patches, the guest kernel does not require any
      modifications, kexec-hardboot is equivalent to standard kexec.
    
      To support dtb, following commit was backported from the mainline:
        commit 4cabd1d9625c7d88acd143f4021fbef75394f154
        Author: Matthew Leach <matthew.leach@arm.com>
        Date:   Fri Sep 21 18:56:02 2012 +0100
    
          ARM: 7539/1: kexec: scan for dtb magic in segments
    
          This patch allows a dtb to be passed to a new kernel using the kexec
          mechinism.
    
          When loading segments from userspace, scan each segment's first four
          bytes for the dtb magic. If this is found set the kexec_boot_atags
          parameter to the relocate_kernel code to the phyical address of this
          segment.
    
      and fixes of this commit:
        c564df4db85aac8d1d65a56176a0a25f46138064
        2456f44dd7a9aaffc2cd21a13f78198b3d94da08
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Change-Id: I2842498f3060173d0b6d64b8c293b0643c96f256
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -36,6 +36,10 @@
 #include <linux/platform_data/lge_android_usb.h>
 #endif
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
+#endif
+
 static int cn_arr_len = 3;
 
 #ifdef CONFIG_LGE_PM_USB_ID
@@ -227,6 +231,8 @@ void __init lge_add_persist_ram_devices(void)
 	/* change to variable value to ram->start value */
 	lge_persist_ram.start = mt->start - LGE_PERSISTENT_RAM_SIZE;
 	pr_info("PERSIST RAM CONSOLE START ADDR : 0x%x\n", lge_persist_ram.start);
+	pr_info("LGE PERSISTENT RAM SIZE : 0x%x\n", LGE_PERSISTENT_RAM_SIZE);
+	pr_info("LGE RAM Console Size : 0x%x\n", LGE_RAM_CONSOLE_SIZE);
 
 	ret = persistent_ram_early_init(&lge_persist_ram);
 	if (ret) {
@@ -238,6 +244,17 @@ void __init lge_add_persist_ram_devices(void)
 
 void __init lge_reserve(void)
 {
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+ 	struct memtype_reserve *mt = &reserve_info->memtype_reserve_table[MEMTYPE_EBI1];
+phys_addr_t start = mt->start - SZ_1M - LGE_PERSISTENT_RAM_SIZE;
+ int ret = memblock_remove(start, SZ_1M);
+ if(!ret)
+ pr_info("Hardboot page reserved at 0x%X\n", start);
+ else
+ pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 #if defined(CONFIG_ANDROID_PERSISTENT_RAM)
 	lge_add_persist_ram_devices();
 #endif
@@ -251,6 +268,7 @@ void __init lge_add_persistent_device(void)
 	/* write ram console addr to imem */
 	lge_set_ram_console_addr(lge_persist_ram.start,
 			LGE_RAM_CONSOLE_SIZE);
+			pr_info("LGE RAM Console Size : 0x%x\n", LGE_RAM_CONSOLE_SIZE);
 #endif
 #endif
 #ifdef CONFIG_PERSISTENT_TRACER
